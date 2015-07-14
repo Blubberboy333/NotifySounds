c4d3d3114a43220a5b8392204c6300a3545483b0
Mak Ho Yin <hoyinm.dev@gmail.com>
2015-07-14 08:20:26 +0800

    Fixed sound problems, and $this->enabled = array??

--- src/NotifySounds/Main.php
+++ src/NotifySounds/Main.php
@@ -30,7 +30,7 @@ class Main extends PluginBase implements Listener{
 	}
 	
 	// Is NotifySounds enabled?
-	private $enabled = array("on");
+	private $enabled = true;
 	
 	// Command to disable / enable NotifySounds
 	public function onCommand(CommandSender $sender, Command $command, $label, array $args){
@@ -43,20 +43,20 @@ class Main extends PluginBase implements Listener{
 					return false;
 				}else{
 					if($args[0] == "on"){
-						if($this->enabled[0] == "on"){
+						if($this->enabled === true){
 							$sender->sendMessage("NotifySounds are already enabled!");
 							return true;
 						}else{
-							$this->enabled[0] = "on";
+							$this->enabled = true;
 							$sender->sendMessage("NotifySounds enabled!");
 							return true;
 						}
 					}elseif($args[0] == "off"){
-						if($this->enabled[0] == "off"){
+						if($this->enabled[0] === false){
 							$sender->sendMessage("NotifySounds are already disabled!");
 							return true;
 						}else{
-							$this->enabled[0] = "off";
+							$this->enabled[0] = false;
 							$sender->sendMessage("NotifySounds disabled");
 						}
 					}else{
@@ -71,11 +71,14 @@ class Main extends PluginBase implements Listener{
 	public function onPlayerChatEvent(PlayerChatEvent $event){
 		$player = $event->getPlayer()->getDisplayName();
 		$sound = $this->getConfig()->get("Sound");
-		if($this->enabled[0] == "on"){
+		if($this->enabled === true){
 			foreach($this->getServer()->getOnlinePlayers() as $p){
 				if($p !== $player){
 					if($p->hasPermission("notify") || $p->hasPermission("notify.sound")){
-						$p->getLevel()->addSound(new ClickSound($p));
+//Only attack&heal(Close his chat box) the one who sends message to the chat
+$player->attack(1);
+$player->heal(1);
+//Add sound to all players						$p->getLevel()->addSound(new ClickSound($p), $this->getServer()->getOnlinePlayers());
 					}
 				}
 			}
